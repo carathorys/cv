a82c4ae558d21e6f6df22b99facd1c853584ce64
Balazs.Gallay <Balazs.Gallay@providenthu.onmicrosoft.com>
2021-06-21 09:16:54 +0200

    ng update done

--- src/app/educations/educations.component.spec.ts
+++ src/app/educations/educations.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EducationsComponent } from './educations.component';
 
@@ -6,7 +6,7 @@ describe('EducationsComponent', () => {
   let component: EducationsComponent;
   let fixture: ComponentFixture<EducationsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EducationsComponent ]
     })
